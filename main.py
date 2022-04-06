f3b7e4783e6cbbefd331b05e45776b25b1d3d5e2
Lana-abdel <57497028+Lana-abdel@users.noreply.github.com>
2022-04-05 21:27:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 UNK_CUTOFF = 2 
 
-#method that converts words that appear less times than the UNK_CUTOFF to <UNK> (for already preprocessed data)
+#method that converts words that appear less times than the UNK_CUTOFF to <UNK> (used only if preprocessed training set is given in the commandline)
 def changeTrainingUNK(sampleInput):
     specificWordCount = {} #word type count prior to normalization
     tempIn = open(sampleInput, "rt").readlines()
